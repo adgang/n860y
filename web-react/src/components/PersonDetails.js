18530d15bcd1eb177f89471abbcae441b762e591
Aditya Gangipamula <adgang@tuta.io>
2021-06-08 03:00:33 +0530

    added person details as books and meetings

--- web-react/src/components/PersonDetails.js
+++ web-react/src/components/PersonDetails.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
@@ -8,6 +8,8 @@ import { useParams } from 'react-router-dom'
 import { useQuery, gql } from '@apollo/client'
 import Title from './Title'
 // import moment from 'moment'
+
+import { AppBar, Tab, Tabs } from '@material-ui/core'
 
 const GET_PROFILE_QUERY = gql`
   query getProfile($name: String!) {
@@ -34,49 +36,71 @@ export default function PersonDetails() {
   const { loading, error, data } = useQuery(GET_PROFILE_QUERY, {
     variables: { name },
   })
+  const [value, setValue] = useState(0)
+
   if (error) return <p>Error</p>
   if (loading) return <p>Loading</p>
+
+  function a11yProps(index) {
+    return {
+      id: `nav-tab-${index}`,
+      'aria-controls': `nav-tabpanel-${index}`,
+    }
+  }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
 
   return (
     <React.Fragment>
       <Title>{name}</Title>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Book</TableCell>
-            <TableCell>Year</TableCell>
-            <TableCell>Publisher</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.people[0].books.map((row) => (
-            <TableRow key={row.title}>
-              <TableCell>{row.title}</TableCell>
-              <TableCell>{row.year}</TableCell>
-              <TableCell>{row.publisher}</TableCell>
+      <AppBar position="static">
+        <Tabs value={value} onChange={handleChange} aria-label="Tabs">
+          <Tab label="Books" {...a11yProps(0)} />
+          <Tab label="Meetings" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <div key={0} hidden={value !== 0}>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell key="Book">Book</TableCell>
+              <TableCell key="Year">Year</TableCell>
+              <TableCell>Publisher</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Occasion</TableCell>
-            <TableCell>During</TableCell>
-            <TableCell>Place</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.people[0].meetings.map((row) => (
-            <TableRow key={row.title}>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.during}</TableCell>
-              <TableCell>{row.at.name}</TableCell>
+          </TableHead>
+          <TableBody>
+            {data.people[0].books.map((row) => (
+              <TableRow key={row.title}>
+                <TableCell>{row.title}</TableCell>
+                <TableCell>{row.year}</TableCell>
+                <TableCell>{row.publisher}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <div hidden={1 !== value} key={1}>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Occasion</TableCell>
+              <TableCell>During</TableCell>
+              <TableCell>Place</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {data.people[0].meetings.map((row) => (
+              <TableRow key={row.title}>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.during}</TableCell>
+                <TableCell>{row.at.name}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </React.Fragment>
   )
 }
