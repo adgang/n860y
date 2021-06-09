85920c49ed3ccf092d73d7513a61bb42796d28af
Aditya Gangipamula <adgang@tuta.io>
2021-06-10 02:50:13 +0530

    added react pdf integration

--- web-react/src/components/Notes.js
+++ web-react/src/components/Notes.js
@@ -12,6 +12,67 @@ import {
 } from '@material-ui/core'
 import Card from '@material-ui/core/Card'
 import { Tab, Tabs } from '@material-ui/core'
+import { PDFViewer } from '@react-pdf/renderer'
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Font,
+} from '@react-pdf/renderer'
+
+Font.register({
+  family: 'Oswald',
+  src: 'https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf',
+})
+const styles = StyleSheet.create({
+  body: {
+    paddingTop: 35,
+    paddingBottom: 65,
+    paddingHorizontal: 35,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    fontFamily: 'Oswald',
+  },
+  author: {
+    fontSize: 12,
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+  subtitle: {
+    fontSize: 18,
+    margin: 12,
+    fontFamily: 'Oswald',
+  },
+  text: {
+    margin: 12,
+    fontSize: 14,
+    textAlign: 'justify',
+    fontFamily: 'Times-Roman',
+  },
+  image: {
+    marginVertical: 15,
+    marginHorizontal: 100,
+  },
+  header: {
+    fontSize: 12,
+    marginBottom: 20,
+    textAlign: 'center',
+    color: 'grey',
+  },
+  pageNumber: {
+    position: 'absolute',
+    fontSize: 12,
+    bottom: 30,
+    left: 0,
+    right: 0,
+    textAlign: 'center',
+    color: 'grey',
+  },
+})
 
 export default function Notes() {
   const notes = useContext(NoteContext)
@@ -20,6 +81,8 @@ export default function Notes() {
     additions.push(useRef(''))
   }
   const [value, setValue] = useState(0)
+  const [resetState, setResetState] = useState({})
+  console.log(resetState)
 
   function a11yProps(index) {
     return {
@@ -30,21 +93,13 @@ export default function Notes() {
 
   const handleChange = (event, newValue) => {
     setValue(newValue)
+    setResetState({})
   }
-
-  // function Snipper(props) {
-  //   if (props.note.meeting) {
-
-  //   } else {
-
-  //   }
-  //   return
-  // }
 
   function getReferences() {
     return notes.map((n, index) => {
       return (
-        <p key={'reference' + index}>
+        <Text key={'reference' + index}>
           [{index + 1}]
           {' ' +
             (n.meeting
@@ -52,7 +107,7 @@ export default function Notes() {
                   ', '
                 )
               : [n.book.title, n.book.year].join(', '))}
-        </p>
+        </Text>
       )
     })
   }
@@ -64,7 +119,39 @@ export default function Notes() {
       textList.push(notes[i].statement)
       textList.push(additions[i + 1].current.value)
     }
-    return textList.map((t, index) => <p key={'para-' + index}>{t}</p>)
+    return textList.map((t, index) => <Text key={'para-' + index}>{t}</Text>)
+  }
+
+  function getDocument() {
+    return notes.length > 0 ? (
+      <div hidden={value !== 1}>
+        <PDFViewer>
+          <Document>
+            <Page size="A4" style={styles.page}>
+              <View style={styles.section}>{getBody()}</View>
+              <View style={styles.section}>
+                <Text style={styles.subtitle}>References:</Text>
+                {getReferences()}
+              </View>
+            </Page>
+          </Document>
+        </PDFViewer>
+        <br />
+        <p>Right Click And Save As to Download PDF</p>
+      </div>
+    ) : (
+      <div hidden={value != 1}>
+        <PDFViewer>
+          <Document hidden={value !== 1}>
+            <Page size="A4" style={styles.page}>
+              <View style={styles.section}>
+                <Text>Empty</Text>
+              </View>
+            </Page>
+          </Document>
+        </PDFViewer>
+      </div>
+    )
   }
 
   return (
@@ -100,19 +187,8 @@ export default function Notes() {
           )
         })}
         <Divider></Divider>
-        <Button>Save</Button>
       </div>
-
-      {notes.length > 0 ? (
-        <div key={1} hidden={value !== 1}>
-          {getBody()}
-          <br />
-          References:
-          {getReferences()}
-        </div>
-      ) : (
-        ''
-      )}
+      {getDocument()}
     </React.Fragment>
   )
 }
